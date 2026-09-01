a14910e5ab9bc0d36a172adf97687eb82fa17558
ewijah <edgordon2023@gmail.com>
2026-08-31 20:19:49 -0400

    add selectProject function

--- src/DOM.js
+++ src/DOM.js
@@ -1,16 +1,38 @@
-export { renderProjectList }
+export { renderProjectList, renderTaskList, selectProject }
 
 const sidebar = document.querySelector("#sidebar");
 const mainContent = document.querySelector("#main-content");
+let currentProject = null;
 
-function renderProjectList(manager, container) {
+function renderProjectList(manager, container, taskContainer) {
     container.innerHTML = "";
     const projects = manager.getAllProjects();
     for (let i = 0; i < projects.length; i++) {
         const listItem = document.createElement("li");
         listItem.textContent = projects[i].name;
         container.appendChild(listItem);
+        listItem.addEventListener("click", () => {
+            selectProject(projects[i], taskContainer);
+        })
+
+    }
+}
+
+function renderTaskList(project, container) {
+    container.innerHTML = "";
+    const tasks = project.tasks;
+
+    for (let i = 0; i < tasks.length; i++) {
+        const listItem = document.createElement("li");
+        listItem.textContent = ` ${tasks[i].title} - ${tasks[i].formattedDate()};`
+        container.appendChild(listItem);
 
     }
 
 }
+
+function selectProject(project, taskContainer) {
+    currentProject = project;
+    renderTaskList(project, taskContainer);
+}
+
--- src/index.js
+++ src/index.js
@@ -4,13 +4,21 @@ import { ProjectManager } from "./ProjectManager.js";
 import  { Task } from "./Task.js";
 import { Project } from "./Project.js";
 import { loadFromStorage, saveToStorage } from "./storage.js";
-import { renderProjectList } from "./DOM.js";
+import { renderProjectList, renderTaskList, selectProject } from "./DOM.js";
 
 
 const manager = loadFromStorage();
 const sidebar = document.getElementById("sidebar");
-let currentProject = null;
-renderProjectList(manager, sidebar);
+const mainContent = document.getElementById("main-content");
+
+
+renderProjectList(manager, sidebar, mainContent);
+
+const firstProject = manager.getAllProjects()[0];
+if (firstProject) {
+    currentProject = firstProject;
+    selectProject(firstProject, mainContent);
+}
 
 
 
